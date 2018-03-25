bae40b37b7f8d7b76174808bc049b24c0f39f74a
Dreams <memdreams@me.com>
2018-03-24 22:04:56 -0400

    till R-2.15

---
+++ DataStructurePythonCode/SingleList.py
@@ -0,0 +1,211 @@
+""" Implement a single list data structure.
+
+    2018.02.03
+    Jie
+"""
+
+class Node(object):
+    def __init__(self, val, p=0):
+        self.data = val
+        self.next = p
+
+class LinkList(object):
+    def __init__(self):
+        self.head = 0
+        self.tail = 0
+
+    def __getitem__(self, item):
+        if self.is_empty():
+            print('linklist is empty.')
+            return
+        elif item < 0 or item > self.getlength():
+            print('The given key is error.')
+            return
+        else:
+            return self.getItem(item)
+
+    def __setitem__(self, key, value):
+        if self.is_empty():
+            print('linklist is empty.')
+            return
+        elif key < 0 or key > self.getlength():
+            print('The given key is error.')
+            return
+        else:
+            self.delete(key)
+            return self.insert(key)
+
+    def initList(self, data):
+        self.head = Node(data[0])
+        self.tail = Node(data[-1])
+
+        p = self.head
+
+        for i in data[1:-1]:
+            node = Node(i)
+            p.next = node
+            p = p.next
+
+        p.next = self.tail
+
+    def getLength(self):
+        p = self.head
+        length = 0
+        while p!=0:
+            length += 1
+            p = p.next
+        return length
+
+    def is_empty(self):
+        if self.getLength() == 0:
+            return True
+        else:
+            return False
+
+    def clear(self):
+        self.head = 0
+
+    def append(self, item):
+        q = Node(item)
+        if self.head == 0:
+            self.head = q
+            self.tail = q
+        else:
+            p = self.tail
+            p.next = q
+            self.tail = q
+
+    def getItem(self, index):
+        if self.is_empty():
+            print('linklist is empty.')
+            return
+        j = 0
+        p = self.head
+        while p.next != 0 and j<index:
+            p = p.next
+            j += 1
+
+        if j==index:
+            return p.data
+        else:
+            print('Target is not exist.')
+
+    def insert(self, index, newData):
+
+        if self.is_empty() or index < 0 or index > self.getLength():
+            print('Linklist is empty.')
+            return
+
+        if index == 1:
+            q = Node(newData, self.head)
+            self.head = q
+
+        p = self.head
+        post = self.head
+        j = 0
+        while p.next != 0 and j < index:
+            post = p
+            p = p.next
+            j += 1
+
+        if index == j:
+            node = Node(newData, p)
+            post.next = node
+
+
+
+    def delete(self, index):
+        if self.is_empty() or index < 0 or index > self.getLength():
+            print('Linklist is empty.')
+            return
+
+        if index == 0:
+            self.head = self.head.next
+
+        p = self.head
+        post = self.head
+        j = 0
+        while p.next != 0 and j < index:
+            post = p
+            p = p.next
+            j += 1
+
+        if index == j:
+            post.next = p.next
+
+    def index(self, value):
+        if self.is_empty():
+            print('linklist is empty.')
+            return
+
+        p = self.head
+        i = 0
+        while p.next!=0 and not p.data == value:
+            p = p.next
+            i+=1
+
+        if p.data == value:
+            return i
+        else:
+            return -1
+
+
+l = LinkList()
+l.initList([i for i in range(1,6)])
+# print([i for i in range(1,6)])
+print(l.getItem(4))
+l.append(7)
+print(l.getItem(5))
+
+l.insert(4, 10)
+print(l.getItem(3))
+print(l.getItem(4))
+print(l.getItem(5))
+# print(l.getItem(6))
+
+l.delete(5)
+print(l.getItem(5))
+
+l.index(5)
+
+
+
+# Test a loop in linked list
+class ListNode(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    @classmethod  # the decorator use as a Factory
+    def count(cls, a):
+        return cls(a)
+
+def isCycled(head):
+    slowP = fastP = head
+    n = 0
+    while (slowP and fastP and fastP.next):
+        slowP = slowP.next
+        fastP = fastP.next.next
+        n += 1
+        if slowP == fastP:
+            print("The loop is found! {} times search.\n".format(n))
+            return True
+
+    print('There is no loop in the linked list!\n')
+    return False
+
+def creatNode():
+    listNodes = []
+    for i in range(10):
+        node = ListNode(i)
+        listNodes.append(node)
+
+    for i, node in enumerate(listNodes):
+        node.next = listNodes[(i+1)%10]
+    return listNodes
+
+# Test Case
+listNodes = creatNode()
+isCycled(listNodes[0])
+
+
--- DataStructurePythonCode/exercise2.py
+++ DataStructurePythonCode/exercise2.py
@@ -18,8 +18,8 @@ def myfunc():
 
 # myfunc = deco(myfunc) # is the same as @deco, only one of them can exist
 
-myfunc()  # only be decorated once
-myfunc()
+# myfunc()  # only be decorated once
+# myfunc()
 
 # 使用内嵌包装函数确保每次myfunc1都被decorator调用
 def deco1(func):
@@ -34,49 +34,10 @@ def myfunc1():
     print(" myfunc1() called.")
     return 'OK'
 
-# myfunc = deco(myfunc) # is the same as @deco, only one of them can exist
+# myfunc1 = deco1(myfunc1) # is the same as @deco1, only one of them can exist
 
-myfunc1()
-myfunc1()
-
-
-# Test a loop in linked list
-class ListNode(object):
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-    @classmethod  # the decorator use as a Factory
-    def count(cls, a):
-        return cls(a)
-
-def isCycled(head):
-    slowP = fastP = head
-    n = 0
-    while (slowP and fastP and fastP.next):
-        slowP = slowP.next
-        fastP = fastP.next.next
-        n += 1
-        if slowP == fastP:
-            print("The loop is found! {} times search.\n".format(n))
-            return True
-
-    print('There is no loop in the linked list!\n')
-    return False
-
-def creatNode():
-    listNodes = []
-    for i in range(10):
-        node = ListNode(i)
-        listNodes.append(node)
-
-    for i, node in enumerate(listNodes):
-        node.next = listNodes[(i+1)%10]
-    return listNodes
-
-# Test Case
-listNodes = creatNode()
-isCycled(listNodes[0])
+# myfunc1()
+# myfunc1()
 
 
 import cProfile # cProfile.run('testfunc()') is used for test the running time of the function
@@ -92,7 +53,6 @@ class Flower(object):
     @property
     def name(self):
         return self._name
-
     @name.setter
     def name(self, val):
         self._name = val
@@ -100,15 +60,139 @@ class Flower(object):
     @property
     def petal(self):
         return self._petal
+    @petal.setter
+    def petal(self, val):
+        self._petal = val
 
     @property
     def price(self):
         return self._price
+    @price.setter
+    def price(self, val):
+        self._price = val
+
+# R-2.5
+class CreditCard:
+    def __init__(self, customer, bank, acnt, limit, balance=0): # R-2.7
+        """
+        Create a new credit card instance.
+
+        :arg
+        customer:   the name of the customer
+        bank:       the name of the bank
+        acnt:       the acount identifier(eg., '1234 1234 5555 5555')
+        limit:      credit limit
+        balance:    the default initial balance is zero.
+        """
+        self._customer = customer
+        self._bank = bank
+        self._account = acnt
+        self._limit = limit
+        self._balance = balance
+
+    def charge(self, price):
+        try:
+            price = float(price)
+        except ValueError:
+            print("The input is not a number.\n")
+
+        if price+self._balance > self._limit:
+            return False
+        else:
+            self._balance += price
+            return True
+
+    def make_payment(self, amount):
+        try:
+            amount = float(amount)
+        except ValueError:
+            print("The input is not a number.\n")
+        self._balance -= amount
+
+# R-2.9 & 2.10
+class Vector:
+    """
+    Represent a vector in a multidimensional space
+    """
+    # R-2.15
+    def __init__(self, d):
+        if type(d) is int:
+            self._coords = [0]*d
+        else:
+            self._coords = d
+
+    def __len__(self):
+        return len(self._coords)
+
+    def __getitem__(self, item):
+        return self._coords[item]
+
+    def __setitem__(self, key, value):
+        self._coords[key] = value
+
+    def __add__(self, other):
+        """Return sum of two vectors"""
+        if len(self) != len(other):
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] + other[j]
+        return result
+
+    #2.11
+    def __radd__(self, other):
+        """Return sum of two vectors"""
+        return self.__add__(other)
+
+    # 2.9
+    def __sub__(self, other):
+        """Return subtraction of two vectors"""
+        if len(self) != len(other):
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] - other[j]
+        return result
+
+    # 2.10
+    def __neg__(self):
+        result = Vector(len(self))
+        for i in range(len(self)):
+            result[i] = -self[i]
+        return result
+
+    # 2.12 & 2.14
+    def __mul__(self, other):
+        if type(other) is int:
+            result = Vector(len(self))
+            for i in range(len(self)):
+                result[i] = self[i] * other
+            return result
+        else:
+            if len(self) != len(other):
+                raise ValueError('dimensions must agree!')
+            result = 0
+            for j in range(len(self)):
+                result += self[j] * other[j]
+            return result
+
+    # 2.13
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    a = Flower()
-    b = Flower('iry', 3, 10)
-    a.name = 'lily'
-    print(b.petal)
+    v = Vector(5)
+    v[1] = 4
+    v[2] = 5
+    u = Vector([1,2,3,4,5])
+    x = u + v
+    print(x._coords)
 
