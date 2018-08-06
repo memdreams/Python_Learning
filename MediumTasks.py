8bd4ccb6d767d4efb94c2709bbc026a08f421018
memdreams <memdreams@me.com>
2018-08-06 17:38:55 -0400

    add 385. Mini Parser

--- MediumTasks.py
+++ MediumTasks.py
@@ -74,3 +74,81 @@ class Solution:
         return m << shift
     
     
+    ## 385. Mini Parser
+    # """
+    # This is the interface that allows for creating nested lists.
+    # You should not implement it, or speculate about its implementation
+    # """
+    #class NestedInteger:
+    #    def __init__(self, value=None):
+    #        """
+    #        If value is not specified, initializes an empty list.
+    #        Otherwise initializes a single integer equal to value.
+    #        """
+    #
+    #    def isInteger(self):
+    #        """
+    #        @return True if this NestedInteger holds a single integer, rather than a nested list.
+    #        :rtype bool
+    #        """
+    #
+    #    def add(self, elem):
+    #        """
+    #        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+    #        :rtype void
+    #        """
+    #
+    #    def setInteger(self, value):
+    #        """
+    #        Set this NestedInteger to hold a single integer equal to value.
+    #        :rtype void
+    #        """
+    #
+    #    def getInteger(self):
+    #        """
+    #        @return the single integer that this NestedInteger holds, if it holds a single integer
+    #        Return None if this NestedInteger holds a nested list
+    #        :rtype int
+    #        """
+    #
+    #    def getList(self):
+    #        """
+    #        @return the nested list that this NestedInteger holds, if it holds a nested list
+    #        Return None if this NestedInteger holds a single integer
+    #        :rtype List[NestedInteger]
+    #        """
+    
+    # actually, in python, this function can just be implemented by eval(s)
+    def deserialize(self, s):
+        """
+        :type s: str
+        :rtype: NestedInteger
+        """
+        if not s:
+            return NestedInteger()
+        if s[0] != '[':
+            return NestedInteger(int(s))
+        
+        
+        nestedS = NestedInteger()
+        stackList = []
+        digits, neg = None, 1
+        
+        for c in s:
+            if c == '-':
+                neg = -1
+            elif c.isdigit():
+                digits = (digits or '') + c
+            if c == '[':
+                stackList.append(NestedInteger())
+            else:
+                if digits is not None:
+                    stackList[-1].add(NestedInteger(int(digits) * neg))
+                    digits, neg = None, 1
+                if c == ']':
+                    top = stackList.pop()
+                    if stackList:
+                        stackList[-1].add(top)
+                    else:
+                        return top
+    
